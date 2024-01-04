cc6708bc1cd626c9187ed7088aaf0556155a547c
king <112244652+uuell@users.noreply.github.com>
2024-01-04 09:45:28 +0800

    Initial commit for pushing to github

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
+  // the layout component receives a children prop which is this
+  // this child can either be a layout or pages inside the /app
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
